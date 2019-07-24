09f5b97486ac3add56c945ad97421a4d16252f0e
milosdev0805 <mstefanovicand@gmail.com>
2019-07-24 11:31:38 -0700

    Add images to preload cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,12 @@ module.exports = {
         icon: `src/branding/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-offline`
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        globPatterns: ['**/*.{js,jpg,html,css}'],
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
