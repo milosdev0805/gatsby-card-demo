e9eaac6061b3ef812415aca5a822fc018561dc9f
milosdev0805 <mstefanovicand@gmail.com>
2019-08-15 14:03:51 -0700

    Change background color of icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/branding/ragan-icon.png`, // This path is relative to the root of the site.
+        icon: `src/branding/ragan-icon-white.png`, // This path is relative to the root of the site.
       },
     },
     {
